41aa9cfda72899dbf68d14ebcf7d72e848c45a6f
wanchatpookhuntod <exandwhy666@yahoo.com>
2021-02-18 10:47:38 +0700

    umm

--- demo.py
+++ demo.py
@@ -5,7 +5,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 answers = pd.read_excel('answerExam.xlsx')
-img = cv2.imread('image/20210216_095847.jpg')
+img = cv2.imread('t_image1.jpg')
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 
 def nothing(x):
@@ -15,21 +16,26 @@ def nothing(x):
 cv2.namedWindow('image')
 
 # create trackbars for color change
-cv2.createTrackbar('MI','image',0,255,nothing)
-cv2.createTrackbar('MX','image',0,255,nothing)
+cv2.createTrackbar('min','image',100,300,nothing)
+cv2.createTrackbar('max','image',0,100,nothing)
 
 # create switch for ON/OFF functionality
 
 while True:
 
-    mi = cv2.getTrackbarPos('MI', 'image')
-    mx = cv2.getTrackbarPos('MX', 'image')
+    mi = cv2.getTrackbarPos('min', 'image')
+    mx = cv2.getTrackbarPos('max', 'image')
+    #
+    # ret, thresh2 = cv2.threshold(img, mi, mx, cv2.THRESH_BINARY_INV)
+    # equalized = cv2.equalizeHist(gray)
+    # alpha = 3 # Contrast control (1.0-3.0)
+    # beta = 0  # Brightness control (0-100)
 
-    ret, thresh2 = cv2.threshold(img, mi, mx, cv2.THRESH_BINARY_INV)
+    # adjusted = cv2.convertScaleAbs(gray, alpha=1.23, beta=33)
+    _, resultThresh = cv2.threshold(gray, mi, mx, cv2.THRESH_BINARY_INV)
+    cv2.imshow('image', resultThresh)
 
-    cv2.imshow('image', thresh2)
-
-    print(f'min:{mi} | max{mx}')
+    print(f'alpha:{mi} | beta{mx}')
 
     if cv2.waitKey(1) == 27:
         break
